32c8b209822d7f8707ea6421700a6dfd14a6d169
guest03 <guest03@pc7-129-l.cs.st-andrews.ac.uk>
2020-03-07 16:21:20 +0000

    Maybe?

--- backend/hardware/servo.py
+++ backend/hardware/servo.py
@@ -7,8 +7,10 @@ GPIO.setup(servoPIN, GPIO.OUT)
 
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
+print("Setting up")
 try:
   while True:
+    print("Run cycle")
     p.ChangeDutyCycle(5)
     time.sleep(0.5)
     p.ChangeDutyCycle(7.5)
