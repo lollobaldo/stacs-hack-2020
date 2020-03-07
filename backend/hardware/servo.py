7ae858a8f8e795aeba0c06b4a15e3a30a2d8cd6c
guest03 <guest03@pc7-129-l.cs.st-andrews.ac.uk>
2020-03-07 16:54:11 +0000

    hope

--- backend/hardware/servo.py
+++ backend/hardware/servo.py
@@ -1,13 +1,21 @@
 import RPi.GPIO as GPIO
 from time import sleep #import time
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
+# servoPIN = 17
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+# p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+# p.start(2.5) # Initialization
 print("Setting up")
+
+GPIO.setup(3, GPIO.OUT)
+# Now setup PWM on pin #3 at 50Hz
+
+pwm=GPIO.PWM(3, 50)
+# Then start it with 0 duty cycle so it doesn't set any angles on startup
+
+pwm.start(0)
 
 def SetAngle(angle):
 	duty = angle / 18 + 2
