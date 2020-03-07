bd122551d5042af6ca51c87f3308c26421202224
guest03 <guest03@pc7-129-l.cs.st-andrews.ac.uk>
2020-03-07 16:26:49 +0000

    Maybe?

--- backend/hardware/try.py
+++ backend/hardware/try.py
@@ -4,7 +4,8 @@ GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD
 GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
   
 try:  
-    while True:  
+    while True:
+        print("Loop")
         GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
         sleep(0.5)                 # wait half a second  
         GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
