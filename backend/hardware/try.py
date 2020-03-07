7d025f2e78558c01ceed0983c6dd26a5b06f7063
guest03 <guest03@pc7-129-l.cs.st-andrews.ac.uk>
2020-03-07 16:24:08 +0000

    Maybe?

---
+++ backend/hardware/try.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO            # import RPi.GPIO module  
+from time import sleep             # lets us have a delay  
+GPIO.setmode(GPIO.BCM)             # choose BCM or BOARD  
+GPIO.setup(17, GPIO.OUT)           # set GPIO24 as an output   
+  
+try:  
+    while True:  
+        GPIO.output(17, 1)         # set GPIO24 to 1/GPIO.HIGH/True  
+        sleep(0.5)                 # wait half a second  
+        GPIO.output(17, 0)         # set GPIO24 to 0/GPIO.LOW/False  
+        sleep(0.5)                 # wait half a second  
+  
+except KeyboardInterrupt:          # trap a CTRL+C keyboard interrupt  
+    GPIO.cleanup()                 # resets all GPIO ports used by this program  
