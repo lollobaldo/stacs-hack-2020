6c40dded8375548a8590296f0f9e326ff688f452
guest03 <guest03@pc7-129-l.cs.st-andrews.ac.uk>
2020-03-07 16:50:11 +0000

    hope

--- backend/hardware/servo.py
+++ backend/hardware/servo.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-import time
+from time import sleep #import time
 
 servoPIN = 17
 GPIO.setmode(GPIO.BCM)
@@ -8,25 +8,43 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
 p.start(2.5) # Initialization
 print("Setting up")
-try:
-  while True:
-    print("Run cycle")
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(12.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(10)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(7.5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(5)
-    time.sleep(0.5)
-    p.ChangeDutyCycle(2.5)
-    time.sleep(0.5)
-except KeyboardInterrupt:
-  p.stop()
-  GPIO.cleanup()
+
+def SetAngle(angle):
+	duty = angle / 18 + 2
+	GPIO.output(3, True)
+	p.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(3, False)
+	p.ChangeDutyCycle(0)
+
+SetAngle(90) 
+sleep(1)
+SetAngle(0)
+sleep(1)
+SetAngle(180)
+
+pwm.stop()
+GPIO.cleanup()
+
+# try:
+#   while True:
+#     print("Run cycle")
+#     p.ChangeDutyCycle(5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(7.5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(10)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(12.5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(10)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(7.5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(5)
+#     time.sleep(0.5)
+#     p.ChangeDutyCycle(2.5)
+#     time.sleep(0.5)
+# except KeyboardInterrupt:
+#   p.stop()
+#   GPIO.cleanup()
