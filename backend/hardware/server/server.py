0b8145ac4e19ae9eb4dacfc445402ae893be9ed3
lollobaldo <lollobaldo2000@gmail.com>
2020-03-08 03:22:29 +0100

    Working

--- backend/hardware/server/server.py
+++ backend/hardware/server/server.py
@@ -1,80 +1,100 @@
-import configparser, logging, os, sys
+#!/usr/bin/python
+import RPi.GPIO as GPIO
 from time import sleep
-import paho.mqtt.client as mqtt
-
-from utils import my_logger
-import modules.DHT as DHT
-import modules.systemStats as SS
-import modules.remoteCommands as RC
-import modules.lights as LS
-
-#read config from file
-config = configparser.RawConfigParser()
-config.read("config.ini")
-PRGM_LOGGING_FILE    = config.get("MAIN", "prgm_logging_file")
-PRGM_LOGGING_FORMAT  = config.get("MAIN", "prgm_logging_format")
-PRGM_LOGGING_CHANNEL = config.get("MAIN", "prgm_logging_channel")
+import paho.mqtt.client as mqtt #import the client1
+import pigpio
 
 
-#read credentials
-creds = configparser.RawConfigParser()
-creds.read("credentials.ini")
-MQTT_SERVER   = creds.get("MAIN","serv")
-MQTT_USERNAME = creds.get("MAIN","user")
-MQTT_PASSWORD = creds.get("MAIN","pswd")
-MQTT_PORT     = creds.getint("MAIN","port")
-
-#logging.basicConfig(format=PRGM_LOGGING_FORMAT, filename=PRGM_LOGGING_FILE,
-#						level=logging.INFO)
-
-#list of modules to initialise
-modules_list = [DHT, SS, RC, LS]
-
-#placeholders for subscriptions and error-dealing
-subscribed_handlers = {}
-at_exception = []
-
-#setup modules
-def setup():
-	#client.subscribe()
-	#load channels from modules
-	for module in modules_list:
-		module.setup(client)
-		subscribed_handlers.update(module.TO_SUBSCRIBE)
-		at_exception.append(module.cleanup)
-	for key in subscribed_handlers.keys():
-		client.subscribe(key)
-
-#on MQTT connection
 def on_connect(client, userdata, flags, rc):
-	my_logger(client , PRGM_LOGGING_CHANNEL , "Connected with result code " + str(rc))
+	print("Connected")
+	print("Subscribing to topic","ibin/throw")
+	client.subscribe("ibin/throw")
 	client.publish(PRGM_LOGGING_CHANNEL , "main.py launched correctly" , 2 , True)
-	setup()
 
-#forward message to module subscribing to it
 def on_message(client, userdata, msg):
-	topic = str(msg.topic)
-	payload_message = str(msg.payload.decode("utf-8"))
-	my_logger(client , PRGM_LOGGING_CHANNEL , "Message received in topic " + topic + ": " + payload_message)
-	subscribed_handlers[topic](payload_message)
+	side = str(msg.payload.decode("utf-8"))
+	print(msg.topic+" "+side)
+	if side == "0":
+		throwLeft()
+	elif side == "2":
+		throwRight()
+	else:
+		print("Input valid side.")
 
-#client log
-def on_log(client, userdata, level, buf):
-	my_logger(client , PRGM_LOGGING_CHANNEL , "Log: " + buf)
-	pass
+# # # #forward message to module subscribing to it
+# def on_message(client, userdata, msg):
+# 	topic = msg.topic
+# 	payload_message = str(msg.payload.decode("utf-8"))
+# 	print("New message on topic ",topic, payload)
+# 	# if side == "0":
+# 	# 	throwLeft()
+# 	# elif side == "2":
+# 	# 	throwRight()
+# 	# else:
+# 	# 	print("Input valid side.")
 
+print("creating new instance")
 client = mqtt.Client()
-client.username_pw_set(MQTT_USERNAME,MQTT_PASSWORD)
 client.on_connect = on_connect
 client.on_message = on_message
-client.on_log     = on_log
-client.connect(MQTT_SERVER,MQTT_PORT,60)
+client.username_pw_set("bisavwqs","gspBl5b8VeTt")
+client.connect("m21.cloudmqtt.com", 16973, 60)
 client.loop_start()
 
+
+#########################################
+### Thrower
+
+#!/usr/bin/env python
+
+
+pi = pigpio.pi() # Connect to local Pi.
+
+
+def throwLeft():
+	print("Throwing left.")
+	pi.set_servo_pulsewidth(18, 500)
+	sleep(0.5)
+	pi.set_servo_pulsewidth(18, 1250)
+	sleep(0.5)
+	pi.set_servo_pulsewidth(18, 0)
+
+def throwRight():
+	print("Throwing right.")
+	pi.set_servo_pulsewidth(18, 2000)
+	sleep(0.5)
+	pi.set_servo_pulsewidth(18, 1250)
+	sleep(0.5)
+	pi.set_servo_pulsewidth(18, 0)
+
+print("Got here")
+try:
+	pi.set_servo_pulsewidth(18, 1250)
+	sleep(0.5)
+	pi.set_servo_pulsewidth(18, 0)
+except KeyboardInterrupt:
+	pwm.stop()
+	GPIO.cleanup()
+
+#########################################
+### FLAME
+
+
+#GPIO SETUP
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+ 
+def callback(channel):
+	print("flame detected")
+	client.publish("ibin/flame","1")
+ 
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
+ 
 while 1:
 	try:
 		sleep(1000)
 	except KeyboardInterrupt:
-		for clean in at_exception:
-			clean()
+		GPIO.cleanup()
 		raise
