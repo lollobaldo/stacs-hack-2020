920e5fb29ac598b3c2524dc5c6612b279a71776c
guest04 <guest04@pc7-088-l.cs.st-andrews.ac.uk>
2020-03-08 04:09:43 +0000

    map with user location positioning

--- client/lib/tabs/2_map.dart
+++ client/lib/tabs/2_map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 class MapSample extends StatefulWidget {
   @override
@@ -10,38 +11,43 @@ class MapSample extends StatefulWidget {
 
 class MapSampleState extends State<MapSample> {
   Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  Location location = Location();
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
         mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
+        initialCameraPosition: _stAndysPos,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
+        onPressed: _goToUser,
+        label: Text('Find Nearby Bins'),
+        icon: Icon(Icons.restore_from_trash),
       ),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  final _getPos = (lat, long) =>  CameraPosition( zoom: 14,
+                                                  tilt: 0, // i.e. camera directly facing the earth
+                                                  bearing: 0, // i.e. camera points the north 
+                                                  target: LatLng(lat, long));
+
+  final _stAndysPos = CameraPosition(zoom: 14,
+                                    tilt: 0, // i.e. camera directly facing the earth
+                                    bearing: 0, // i.e. camera points the north 
+                                    target: LatLng(56.3399085,-2.8096820));
+
+  Future<CameraPosition> _getUserPos() async {
+    final currentLocation = await location.getLocation();
+    return _getPos(currentLocation.latitude, currentLocation.longitude);
+  }
+
+  Future<void> _goToUser() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    _getUserPos().then((pos) => controller.animateCamera(CameraUpdate.newCameraPosition(pos)));
   }
 }
